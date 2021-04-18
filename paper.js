6d1533e3dda159272d83599624db4edc043d912c
avisikta-05 <77844081+avisikta-05@users.noreply.github.com>
2021-04-18 11:33:02 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,25 @@
+class paper
+{
+    constructor(x,y,r){
+        var options= {
+            isStatic:false
+            restitution:0.3
+            friction:0
+            density:1.2
+        }
+        this.body= bodies.circle(x,y,r,options):
+        this.r=r
+        
+        this.image= loadImage("paper.png")
+        World.add(world,this.body);
+    }
+     display(){
+         var pos=this.body.position
+         push();
+         translate(pos.x,pos.y)
+         imageMode(CENTRE)
+         image(this.image,0,0,this.r,this.r)
+         pop():
+
+     }
+}
